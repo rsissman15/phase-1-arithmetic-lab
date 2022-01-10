8b411085aec75ce05399da2fa5ff36103351d742
Rebecca Sissman <rsissman15@aol.com>
2022-01-10 15:39:38 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+ const num1=62;
+ const num2=1;
+ const multiply=num1*num2;
+
+ const random=Math.floor(Math.random() * 10) + 1;
+
+ const num3=32;
+ const num4=14;
+ const mod= num3 % num4;
+
+ const max= Math.max(1, 2, 3, 4, 5,20);
